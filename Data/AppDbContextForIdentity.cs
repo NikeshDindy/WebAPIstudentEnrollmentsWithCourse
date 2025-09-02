d3c3bded91680473bdf76f3a558c1c92ec66307c
NikeshDindy <nikeshdindy222@gmail.com>
2025-09-02 09:29:53 +0530

    these all for the creation of the identity for the authentication

---
+++ Data/AppDbContextForIdentity.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebAPIstudentEnrollmentsWithCourse.Data
+{
+    public class AppDbContextForIdentity : IdentityDbContext<IdentityUser>
+    {
+        public AppDbContextForIdentity(DbContextOptions<AppDbContextForIdentity> options) : base(options) { }
+
+        public AppDbContextForIdentity() { }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,26 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+
+
+
 // Add this using directive to fix CS1061
 using WebAPIstudentEnrollmentsWithCourse.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddAuthorization(); // Adding identity services
+
+// activating Identity APIs
+builder.Services.AddIdentityApiEndpoints<IdentityUser>().AddEntityFrameworkStores<AppDbContextForIdentity>();
+
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<AppDbContextForIdentity>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("AppDbCon")));
+
+
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
@@ -14,6 +28,8 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
+
+app.MapIdentityApi<IdentityUser>(); // mapping identity user
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
